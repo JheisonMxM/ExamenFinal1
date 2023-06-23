cbaec7bb66cfb1a9af4a104f8cd1300bcfca15a3
JheisonMxM <mquispep@est.emi.edu.com.bo>
2023-06-23 11:56:25 -0400

    1ercommitExamenFinal

---
+++ src/components/Descripcion.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Descripcion.css";
+import { Icon } from '@iconify/react';
+export const Card = (props) => {
+    return (
+
+        <div className="text-conatiner">
+           <span className="txt_1">Standard Cactus</span>
+           
+    
+        </div>   
+    
+            
+      )
+};
