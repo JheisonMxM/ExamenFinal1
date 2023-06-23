f062169e2ba135d5e121e56d04ea2d54bef5566b
JheisonMxM <mquispep@est.emi.edu.com.bo>
2023-06-23 12:21:01 -0400

    2docommitExamenFinal

---
+++ src/components/Cantidad.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Cantidad.css";
+import { Icon } from '@iconify/react';
+export const Desc = (props) => {
+    return (
+
+        <div className="cantidad-conatiner">
+          
+        </div>   
+    
+            
+      )
+};
+
